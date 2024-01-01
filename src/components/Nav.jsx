e438da72070e6e50fd76a491d1a07ba49f4eccd5
olanrewaju adebayo <adebayoolanrewaju970@gamail.com>
2024-01-01 16:54:49 +0100

    worked on the nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import {headerLogo} from '../assets/images'
-// import {hambuger} from '../assets/icons'
+import {hamburger} from '../assets/icons'
+import {navLinks} from '../constants'
 
 const Nav = () => {
   return (
@@ -9,9 +10,19 @@ const Nav = () => {
             src={headerLogo}
             alt="logo"
             width={130}
-            height={29}
-             /></a>
-             <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hideen"></ul>
+            height={29}/></a>
+                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                            <a href={item.href} className='font-motnserrat leading-normal text-lg text-slate-gray'>
+                            {item.label}
+                            </a>
+                    </li>
+                ))}
+                </ul>
+                <div className='hidden max-lg:block'>
+                    <img width={25} height={25} src={hamburger} alt="hambuger" />
+                </div>
         </nav>
     </header>
   )
